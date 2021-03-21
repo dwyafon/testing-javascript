af35a0e0038a84c93ebe8ed7105c60f38f52082c
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-03-21 18:40:47 +0000

    add README

--- math.js
+++ math.js
@@ -1,3 +1,9 @@
 const product = (a, b) => a + b
+const sum = (a, b) => a * b
+const doubleNum = (num) => num + num
 
-module.exports = { product }
+const asyncProduct = (...args) => Promise.resolve(product(...args))
+const asyncDoubleNum = (...args) => Promise.resolve(doubleNum(...args))
+
+
+module.exports = { product, sum, doubleNum, asyncProduct, asyncDoubleNum  }
---
+++ test-globals.js
@@ -0,0 +1,27 @@
+function test(title, callback) {
+    try {
+      callback()
+      console.log(`✓ ${title}: passed`)
+    } catch (error) {
+      console.error(`✕ ${title}: ${error}`)
+    }
+  }
+  
+  // returns an object with assertions
+  function expect(actual) {
+    return {
+      toBe(expected) {
+        if (actual != expected) {
+          throw new Error(`${actual} is not equal to ${expected}`)
+        }
+      },
+      toBeStrictlyEqualTo(expected) {
+        if (!(actual === expected)) {
+          throw new Error(`${actual} is not strictly equal to ${expected}`)
+        }
+      },
+    }
+  }
+
+  global.test = test
+  global.expect = expect
